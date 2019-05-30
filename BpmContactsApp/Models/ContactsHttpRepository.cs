906191e6d2573eceadd5f49ad9dd618beca29bd5
Dalatar <dalatarion@gmail.com>
2019-05-30 18:04:26 +0300

    init commit

---
+++ BpmContactsApp/Models/ContactsHttpRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BpmContactsApp.Models
+{
+    public class ContactsHttpRepository : IRepository<Contact>
+    {
+        private AppContext _contextBpm;
+
+        public void Create(Contact item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Contact> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Contact item)
+        {
+            throw new NotImplementedException();
+        }
+       
+
+
+    }
+}
--- BpmContactsApp/Startup.cs
+++ BpmContactsApp/Startup.cs
@@ -24,12 +24,7 @@ namespace BpmContactsApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+         
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -38,20 +33,11 @@ namespace BpmContactsApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+          
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+          
 
             app.UseMvc(routes =>
             {
